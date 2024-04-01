aca6f46884d93adcb3c7f1ab6bb45f2bd6cca0a9
Razvan Timis <r@timis.dev>
2024-04-01 22:40:26 +0300

    Add location and confirmation ( wip )

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind(), partytown()],
 });
