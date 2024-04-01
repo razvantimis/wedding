86d9dcc14158c16be52192077fc5c4422e933a4f
Razvan Timis <r@timis.dev>
2024-04-01 19:32:44 +0300

    Add the initial page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), partytown()]
+});
