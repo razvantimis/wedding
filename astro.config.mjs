b15167cf615b3d3ced951c7702f3d7c524ef8e73
Razvan Timis <r@timis.dev>
2024-04-02 19:50:59 +0300

    Update contact informatie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()],
+  integrations: [
+    tailwind(),
+    partytown(),
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 });
